6886deaffee03f0d393e3aa94dabb965ea3fae5c
osamasaid290 <85513090+osamasaid290@users.noreply.github.com>
2021-06-08 12:48:33 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
+new WOW().init();
 
+/* make map */
+let map;
+let marker;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+/* toggle the class setting box when you click on bag */
+document.querySelector('.fa-shopping-bag').onclick = function(){
+
+  document.querySelector('.setting-box-bag').classList.add('open');
+}
+
+document.querySelector('.offcanvas-close').onclick = function(){
+
+  document.querySelector('.setting-box-bag').classList.remove('open');
+}
+
+/* toggle the class setting box when you click on heart */
+document.querySelector('.fa-heart').onclick = function(){
+
+  document.querySelector('.setting-box-heart').classList.add('open');
+}
+
+document.querySelector('.offcanvas-closee').onclick = function(){
+
+  document.querySelector('.setting-box-heart').classList.remove('open');
+}
+
+/* add search class */
+document.querySelector('.fa-search').onclick = function(){
+
+  document.querySelector('.search').classList.add('search-open');
+}
